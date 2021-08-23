b5baa43f1a094291890fe0e52987dbf32046ef1d
Olawale Otubu <waleoxy@gmail.com>
2021-08-23 16:39:22 +0100

    Calendar implementaion completed...started search button to search pg impl

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,34 @@ import {
     UsersIcon,
     MenuIcon,
 } from '@heroicons/react/solid';
+import { useState } from "react";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
 
+    const [searchIput, setSearchIput] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date())
+    const [noOfGuests, setNoOfGuests] = useState('1')
+
     const myLoader = ({ src, width, quality }) => {
         return `https://links.papareact.com/${src}?w=${width}&q=${quality || 75}`
+    }
+
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection'
+    }
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+    const resetInput = () => {
+        setSearchIput('');
     }
 
     return (
@@ -32,12 +55,15 @@ function Header() {
             <div className="flex items-center md:border-2 
             rounded-full py-2 md:shadow-sm">
                 <input
-                    className="pl-5 bg-transparent 
+                    value={searchIput}
+                    className="pl-5 bg-transparent  
                     flex-grow outline-none text-sm
                     text-gray-600 placeholder-gray-400"
                     type="text"
                     placeholder="start your search"
+                    onChange={(event) => setSearchIput(event.target.value)}
                 />
+                {console.log('my', searchIput)}
                 <SearchIcon className="hidden md:inline-flex 
                 h-8 bg-red-400 text-white 
                 rounded-full p-2 cursor-pointer 
@@ -53,9 +79,44 @@ function Header() {
                 border-2 p-2 rounded-full">
                     <MenuIcon className="h-6 cursor-pointer" />
                     <UserCircleIcon className="h-6 cursor-pointer" />
-
                 </div>
             </div>
+            {searchIput && (
+                <div className="flex flex-col col-span-3 mx-">
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        minDate={new Date()}
+                        rangeColors={["#fd5861"]} />
+                    <div className="flex items-center border-b mb-4">
+                        <h2 className="text-2xl flex-grow font-semibold ">
+                            Number of Guest
+                        </h2>
+                        <UsersIcon className="h-5" />
+                        <input
+                            type="number"
+                            input={noOfGuests}
+                            min={1}
+                            onChange={(event) => setNoOfGuests(event.target.value)}
+                            className="w-12 pl-2
+                            text-lg outline-none text-red-400" />
+                    </div>
+                    <div className="flex">
+                        <button
+                            className="flex-grow text-gray-500"
+                            onClick={resetInput}
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            className="flex-grow text-red-400"
+                        //onClick={}
+                        >
+                            Search
+                        </button>
+                    </div>
+                </div>
+            )}
         </header >
     )
 }
---
+++ pages/search.js
@@ -0,0 +1,20 @@
+const { default: Footer } = require("../components/Footer");
+const { default: Header } = require("../components/Header");
+
+function Search() {
+    return (
+        <div>
+            <h1>I am the search page</h1>
+            <Header />
+            <main className="flex">
+                <section>
+
+                </section>
+
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
